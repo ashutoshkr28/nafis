68d1d13d3233618d21743fe037a9a4f3b064304f
ashutoshkr28 <ashutoshkumarmp3@gmail.com>
2024-11-14 22:38:30 +0530

    lala

--- components/Header.jsx
+++ components/Header.jsx
@@ -34,23 +34,42 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <div className="py-5 px-5 md:px-12 lg:px-28">
-      <div className="flex justify-between items-center">
-        <Image src={assets.logo} width={90} height={90} alt="Company Logo" className="w-[70px] h-[70px] sm:w-auto sm:h-auto" />
-        <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-5 border border-solid border-black shadow-[-7px_7px_0px_#000000]">
-          Get Started <Image src={assets.arrow} width={20} height={20} alt="Arrow" />
+    <div className="py-4 px-4 md:px-8 lg:px-20 xl:px-32 bg-gray-100">
+      <div className="flex flex-wrap justify-between items-center gap-4">
+        {/* Logo */}
+        <Image 
+          src={assets.logo} 
+          width={90} 
+          height={90} 
+          alt="Company Logo" 
+          className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28"
+        />
+        
+        {/* Get Started Button */}
+        <button className="flex items-center gap-2 font-medium py-2 px-3 md:py-3 md:px-6 border border-black shadow-[-4px_4px_0px_#000000] hover:bg-gray-200 transition duration-300">
+          Get Started 
+          <Image src={assets.arrow} width={16} height={16} alt="Arrow Icon" className="w-4 h-4 md:w-5 md:h-5" />
         </button>
       </div>
 
-      <div className="text-center my-8">
-        <h1 className="text-3xl sm:text-5xl font-medium">Latest Update</h1>
-        <p className="mt-4 sm:mt-10 max-w-[740px] mx-auto text-xs sm:text-base">
+      {/* Content Section */}
+      <div className="text-center mt-6 md:mt-10 lg:mt-12 xl:mt-14">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-gray-800">Latest Update</h1>
+        <p className="mt-4 sm:mt-6 lg:mt-8 max-w-lg mx-auto text-sm sm:text-base md:text-lg lg:text-xl">
           New change ipsum dolor sit amet consectetur, adipisicing elit. Earum expedita itaque nostrum quasi, quam repellat perspiciatis perferendis voluptas reiciendis adipisci consequatur repudiandae?
         </p>
 
-        <form className="flex justify-between max-w-[300px] sm:max-w-[400px] scale-90 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]" action="">
-          <input type="email" placeholder="Enter your email" className="pl-4 py-2 outline-none flex-grow" />
-          <button type="submit" className="border-l border-black py-2 px-4 sm:px-8 active:bg-gray-600 active:text-white">
+        {/* Subscription Form */}
+        <form className="flex justify-between items-center max-w-xs sm:max-w-md lg:max-w-lg mx-auto mt-8 md:mt-10 border border-black shadow-[-4px_4px_0px_#000000]" action="">
+          <input 
+            type="email" 
+            placeholder="Enter your email" 
+            className="flex-grow pl-4 py-3 outline-none text-sm sm:text-base"
+          />
+          <button 
+            type="submit" 
+            className="border-l border-black py-3 px-4 sm:px-6 text-xs sm:text-sm md:text-base lg:text-lg bg-gray-100 hover:bg-gray-300 transition duration-300"
+          >
             Subscribe
           </button>
         </form>

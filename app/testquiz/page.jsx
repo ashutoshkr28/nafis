5a806e792cd0dcaf14fa0752d421be0dd79b7c48
ashutoshkr28 <ashutoshkumarmp3@gmail.com>
2024-11-15 10:43:47 +0530

    new up

--- app/testquiz/page.jsx
+++ app/testquiz/page.jsx
@@ -46,11 +46,10 @@ const Testquiz = () => {
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800 mb-4 lg:mb-6">
         Quiz Test Mathematics
       </h1>
-      <Image src={assets.test} alt='' width={600}/>
+      <Image src={assets.test} alt='test' width={600}/>
       <h2 className="text-lg md:text-xl lg:text-2xl mt-4 text-red-500 border border-blue-600 py-3 px-4 rounded hover:bg-lime-700 transition duration-300">
         Choose Your Class
       </h2>
-
       <div className="mt-8 lg:mt-10 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
         <Link href="/class/sixth" className="bg-black text-white py-2 px-4 rounded-sm text-center hover:bg-gray-800 transition">
           6th Class
@@ -68,7 +67,6 @@ const Testquiz = () => {
           10th Class
         </Link>
       </div>
-
       <div className="mt-auto pt-10">
         <Link href="/" className="bg-red-800 text-white py-2 px-6 rounded-md text-lg hover:bg-red-700 transition duration-300">
           Home

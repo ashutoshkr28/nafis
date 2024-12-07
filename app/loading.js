830694dbf1abde80931327e62bc5cd4e45d42a92
ashutoshkr28 <ashutoshkumarmp3@gmail.com>
2024-12-07 18:59:08 +0530

    please wait

--- app/loading.js
+++ app/loading.js
@@ -3,8 +3,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
     return(
-        <div className="w-full min-h-screen">
-        <Skeleton> Please Wait</Skeleton>
+        <div className="w-full min-h-screen ">
+
+        <Skeleton> <b style={{color: "green"}}> Please Wait</b></Skeleton>
         </div>
     )
 }

b8a5e6dc3adb6400a17e6f4868f97af5b345ab5e
ashutoshkr28 <ashutoshkumarmp3@gmail.com>
2025-03-03 14:09:44 +0530

    pic added

--- app/9th-science/biology/chapter2/page.jsx
+++ app/9th-science/biology/chapter2/page.jsx
@@ -324,7 +324,7 @@ const QuestionAnswerPage = () => {
       {/* <Image sr></Image> */}
       <div className='flex justify-between'>
         <div>
-      {/* <Image src={"/image/8th-S-Chap-1.png"} className=' rounded mb-12 transition-all transform hover:animate-shake' width={2500} height={300} alt='img'></Image> */}
+      <Image src={"/image/9th-biology-subjective-chapter-2.png"} className=' rounded mb-12 transition-all transform hover:animate-shake' width={2500} height={300} alt='img'></Image>
       </div>
 
       {/* <div className='border border-x-8  flex flex-col'>
--- app/9th-science/chemistry/chapter3/page.jsx
+++ app/9th-science/chemistry/chapter3/page.jsx
@@ -646,7 +646,7 @@ const questions = [
                     {/* <Image sr></Image> */}
                     <div className='flex justify-between'>
                       <div>
-                    {/* <Image src={"/image/8th-S-Chap-1.png"} className=' rounded mb-12 transition-all transform hover:animate-shake' width={2500} height={300} alt='img'></Image> */}
+                    <Image src={"/image/9th-chemistry-subjective-chapter-3.png"} className=' rounded mb-12 transition-all transform hover:animate-shake' width={2500} height={300} alt='img'></Image>
                     </div>
               
                     {/* <div className='border border-x-8  flex flex-col'>
